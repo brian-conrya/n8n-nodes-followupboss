fa6fbe06dc7a99732faafea7befa941c22bfaccf
Brian Hurley <brian@conrya.com>
2025-12-23 23:34:22 -0500

    Some call improvements

--- nodes/FollowUpBoss/actions/calls/log.operation.ts
+++ nodes/FollowUpBoss/actions/calls/log.operation.ts
@@ -33,63 +33,69 @@ const properties: INodeProperties[] = [
 		description: 'Whether this is an incoming or outgoing call',
 	},
 	{
-		displayName: 'Duration',
-		name: 'duration',
-		type: 'number',
-		default: 0,
-		placeholder: '63',
-		description: 'Length of the call in seconds',
-	},
-	{
-		displayName: 'From Number',
-		name: 'fromNumber',
-		type: 'string',
-		default: '',
-		placeholder: 'e.g. 555-555-5555',
-		description: 'The phone number this call was made from',
-	},
-	{
-		displayName: 'Note',
-		name: 'note',
-		type: 'string',
-		default: '',
-		placeholder: "John didn't have time to talk, call back on friday",
-		description: 'The log message entered for this call',
-	},
-	{
-		displayName: 'Outcome',
-		name: 'outcome',
-		type: 'options',
+		displayName: 'Additional Fields',
+		name: 'additionalFields',
+		type: 'collection',
+		placeholder: 'Add Field',
+		default: {},
 		options: [
-			{ name: 'Bad Number', value: 'Bad Number' },
-			{ name: 'Busy', value: 'Busy' },
-			{ name: 'Interested', value: 'Interested' },
-			{ name: 'Left Message', value: 'Left Message' },
-			{ name: 'No Answer', value: 'No Answer' },
-			{ name: 'Not Interested', value: 'Not Interested' },
+			{
+				displayName: 'Duration',
+				name: 'duration',
+				type: 'number',
+				default: 0,
+				placeholder: '63',
+				description: 'Length of the call in seconds',
+			},
+			{
+				displayName: 'From Number',
+				name: 'fromNumber',
+				type: 'string',
+				default: '',
+				placeholder: 'e.g. 555-555-5555',
+				description: 'The phone number this call was made from',
+			},
+			{
+				displayName: 'Note',
+				name: 'note',
+				type: 'string',
+				default: '',
+				placeholder: "John didn't have time to talk, call back on friday",
+				description: 'The log message entered for this call',
+			},
+			{
+				displayName: 'Outcome',
+				name: 'outcome',
+				type: 'options',
+				options: [
+					{ name: 'Bad Number', value: 'Bad Number' },
+					{ name: 'Busy', value: 'Busy' },
+					{ name: 'Interested', value: 'Interested' },
+					{ name: 'Left Message', value: 'Left Message' },
+					{ name: 'No Answer', value: 'No Answer' },
+					{ name: 'Not Interested', value: 'Not Interested' },
+				],
+				default: 'Interested',
+				description: 'The outcome of the call',
+			},
+			{
+				displayName: 'Recording URL',
+				name: 'recordingUrl',
+				type: 'string',
+				default: '',
+				placeholder: 'e.g. https://example.com/recording.mp3',
+				description: 'The URL for the call recording',
+			},
+			{
+				displayName: 'To Number',
+				name: 'toNumber',
+				type: 'string',
+				default: '',
+				placeholder: 'e.g. 555-555-5555',
+				description: 'The phone number this call was made to',
+			},
+			getUserIdProperty('User', 'userId', false),
 		],
-		default: 'Interested',
-		description: 'The outcome of the call',
-	},
-	{
-		displayName: 'Recording URL',
-		name: 'recordingUrl',
-		type: 'string',
-		default: '',
-		placeholder: 'e.g. https://example.com/recording.mp3',
-		description: 'The URL for the call recording',
-	},
-	{
-		displayName: 'To Number',
-		name: 'toNumber',
-		type: 'string',
-		default: '',
-		placeholder: 'e.g. 555-555-5555',
-		description: 'The phone number this call was made to',
-	},
-	{
-		...getUserIdProperty('User', 'userId', false),
-		description: 'The user that made or received the call (can only be set by administrators, otherwise the currently logged in user\'s ID is used). Choose from the list, or specify an ID.',
 	},
 ];
 
@@ -103,13 +109,7 @@ export async function execute(
 	const personId = toInt(personIdRaw, 'Person ID', this.getNode(), i);
 	const phone = this.getNodeParameter('phone', i) as string;
 	const isIncoming = this.getNodeParameter('isIncoming', i) as boolean;
-	const duration = this.getNodeParameter('duration', i) as number;
-	const fromNumber = this.getNodeParameter('fromNumber', i) as string;
-	const note = this.getNodeParameter('note', i) as string;
-	const outcome = this.getNodeParameter('outcome', i) as string;
-	const recordingUrl = this.getNodeParameter('recordingUrl', i) as string;
-	const toNumber = this.getNodeParameter('toNumber', i) as string;
-	const userIdParam = this.getNodeParameter('userId', i) as IDataObject;
+	const additionalFields = this.getNodeParameter('additionalFields', i) as IDataObject;
 
 	const body: IDataObject = {
 		personId,
@@ -117,26 +117,27 @@ export async function execute(
 		isIncoming,
 	};
 
-	if (duration) {
-		body.duration = duration;
+	if (additionalFields.duration) {
+		body.duration = additionalFields.duration;
 	}
-	if (fromNumber) {
-		body.fromNumber = fromNumber;
+	if (additionalFields.fromNumber) {
+		body.fromNumber = additionalFields.fromNumber;
 	}
-	if (note) {
-		body.note = note;
+	if (additionalFields.note) {
+		body.note = additionalFields.note;
 	}
-	if (outcome) {
-		body.outcome = outcome;
+	if (additionalFields.outcome) {
+		body.outcome = additionalFields.outcome;
 	}
-	if (recordingUrl) {
-		body.recordingUrl = recordingUrl;
+	if (additionalFields.recordingUrl) {
+		body.recordingUrl = additionalFields.recordingUrl;
 	}
-	if (toNumber) {
-		body.toNumber = toNumber;
+	if (additionalFields.toNumber) {
+		body.toNumber = additionalFields.toNumber;
 	}
-	if (userIdParam?.value) {
-		body.userId = toInt(userIdParam.value as string, 'User ID', this.getNode(), i);
+	if (additionalFields.userId) {
+		const userIdRaw = (additionalFields.userId as IDataObject).value as string;
+		body.userId = toInt(userIdRaw, 'User ID', this.getNode(), i);
 	}
 
 	const response = await apiRequest.call(this, 'POST', '/calls', body);
